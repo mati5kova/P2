ea553ef17fe602944a004df729e8ad7bc2a0ea99
Matevž Kovačič <77550206+mati5kova@users.noreply.github.com>
2025-04-25 20:32:18 +0200

    dn08 naloga2

--- dn/dn08/naloga2/naloga2.c
+++ dn/dn08/naloga2/naloga2.c
@@ -2,24 +2,27 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void izpisi(int* t, int len)
+{
+	for(int i = 0; i < len; i++)
+	{
+		if(i % 2 == 0)
+			printf("%d", t[i]);
+		else
+			printf("[%d]", t[i]);
+		if(i != len - 1)
+			printf(" -> ");
+	}
+	printf("\n");
+}
+
 bool igraj(int preostaliZetoni, int k, bool beliNaPotezi, int* poteze, int steviloOdigranihPotez, bool izpisujZmage)
 {
-	
-	
 	if(preostaliZetoni == 0)
 	{
 		if(!beliNaPotezi && izpisujZmage)
 		{
-			for(int i = 0; i < steviloOdigranihPotez; i++)
-			{
-				if(i % 2 == 0)
-					printf("%d", poteze[i]);
-				else
-					printf("[%d]", poteze[i]);
-				if(i != steviloOdigranihPotez - 1)
-					printf(" -> ");
-			}
-		printf("\n");
+			izpisi(poteze, steviloOdigranihPotez);
 		}
 		
 		// vrne true ce je beli zmagal (torej je trenutno crni na potezi)
@@ -31,45 +34,65 @@ bool igraj(int preostaliZetoni, int k, bool beliNaPotezi, int* poteze, int stevi
 		printf("neveljavna poteza\n");//neveljavna poteza
 	}
 	
-	if(beliNaPotezi)
-	{
-		bool zmagalVsajEno = false;
-		for(int i = k; i >= 1; i--)
-		{
-			// ker se i zmanjsuje, ce je trenutno negativna vrednost pogledamo naslednjo
-			if(preostaliZetoni - i < 0) continue;
+	// prvi klic iz maina samo isce ali obstaja taka resitev da bo beli zmagal (prav tako preverjanje v drugem delu (bool beliZmagal pred identicnim klicem ampak z izpisujZmage=true))
+	// nekatere veje drevesa lahko skippamo ker ni pomembno da obiscemo vse, ce se prej izpolnejo dolecni pogoji
+    if (!izpisujZmage) {
+        if (beliNaPotezi) {
+            for (int i = k; i >= 1; --i) {
+                if (preostaliZetoni - i < 0) continue;
+				
+                poteze[steviloOdigranihPotez] = i;
+				bool beliZmaga = igraj(preostaliZetoni - i, k, false, poteze, steviloOdigranihPotez + 1, false);
+                
+				if (beliZmaga) return true;
+            }
+            return false;
+        }
+        else {
+            if (preostaliZetoni - k <= 0) return false;
 			
+            for (int i = 1; i <= k; ++i) {
+                if (preostaliZetoni - i < 0) break;
+				
+                poteze[steviloOdigranihPotez] = i;
+				bool beliZmaga = igraj(preostaliZetoni - i, k, false, poteze, steviloOdigranihPotez + 1, false);
+                
+				if (beliZmaga) return false;
+            }
+            return true;
+        }
+    }
+    
+	// drugi klic iz maina, ideja je da najprej pogledamo ce v tej veji beli zmaga, ce zmaga klicemo ponovno ampak z izpisujZmage=true
+	// + ne skippamo vej ampak pregledamo vse
+    if (beliNaPotezi) {
+        for (int i = k; i >= 1; --i) {
+            if (preostaliZetoni - i < 0) continue;
+           
 			poteze[steviloOdigranihPotez] = i;
-			bool beliJeZmagal = igraj(preostaliZetoni - i, k, !beliNaPotezi, poteze, steviloOdigranihPotez + 1, izpisujZmage);
-			if(beliJeZmagal)
+			bool beliZmaga = igraj(preostaliZetoni - i, k, false, poteze, steviloOdigranihPotez + 1, false);
+			
+			if(beliZmaga)
 			{
-				zmagalVsajEno = true;
+				igraj(preostaliZetoni - i, k, false, poteze, steviloOdigranihPotez + 1, true);
 				break;
 			}
-		}
-		return zmagalVsajEno;
-		
-		
-	} else 
-	{
-		if(preostaliZetoni - k <= 0) { return false; }
-		
-		for(int i = 1; i <= k; i++)
-		{
-			// ker se i povecuje, ce je vmes ze negativna vrednost 
-			// 	nima smisla gledat naprej
-			if(preostaliZetoni - i < 0) break; 
+        }
+    }
+    else {
+        for (int i = 1; i <= k; ++i) {
+            if (preostaliZetoni - i < 0) break;
 			
-			poteze[steviloOdigranihPotez] = i;
-			bool beliJeZmagal = igraj(preostaliZetoni - i, k, !beliNaPotezi, poteze, steviloOdigranihPotez + 1, izpisujZmage);
+            poteze[steviloOdigranihPotez] = i;
+           	bool beliZmaga = igraj(preostaliZetoni - i, k, true, poteze, steviloOdigranihPotez + 1, false);
 			
-			if(!beliJeZmagal)
+			if(beliZmaga)
 			{
-				return false; // vrnemo false (v tem poddrevesu crni vsaj enkrat zmaga)
+				igraj(preostaliZetoni - i, k, true, poteze, steviloOdigranihPotez + 1, true);
 			}
-		}
-		return true;
-	}
+        }
+    }
+    return true;
 }
 
 int main()
@@ -77,21 +100,26 @@ int main()
 	int n, k;
 	scanf("%d %d", &n, &k);
 	
-	bool beliKdajZmaga = false;
+	bool beliZmagalCeloIgro = false;
 	
-	for (int i = k; i >= 1; --i) {
-		if(n - i < 0) continue;
+	for (int i = k; i >= 1 && !beliZmagalCeloIgro; --i) {
+		if(n - i < 0) continue; // ce npr k > n
 		
 		int* poteze = calloc(62, sizeof *poteze);
 		poteze[0] = i;
-		if (igraj(n - i, k, false, poteze, 1, false)) {
+		
+		// ce dobimo nazaj feedback da s trenutnim i-jem beli zmaga v vseh scenarijih
+		// igramo se enkrat ampak tokrat izpisujemo
+		bool beliZmagaVse = igraj(n - i, k, false, poteze, 1, false);
+		
+		if (beliZmagaVse) {
 			igraj(n - i, k, false, poteze, 1, true);
-			beliKdajZmaga = true;
+			beliZmagalCeloIgro = true;
 		}
 		free(poteze);
 	}
 	
-	if (!beliKdajZmaga) {
+	if (!beliZmagalCeloIgro) {
 		printf("CRNI\n");
 	}
 	
